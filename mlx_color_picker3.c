da9bf4bc728ce7db45616c61fa81f585c3565e1d
Geofrey ARGENCE <geargenc@e3r6p10.42.fr>
2018-01-30 18:05:44 +0100

    Projet fonctionnel

--- mlx_color_picker3.c
+++ mlx_color_picker3.c
@@ -6,7 +6,7 @@
 /*   By: geargenc <geargenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/26 15:38:03 by geargenc          #+#    #+#             */
-/*   Updated: 2018/01/30 15:13:13 by geargenc         ###   ########.fr       */
+/*   Updated: 2018/01/30 18:03:01 by geargenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define ABS(x) (((x) < 0) ? ((x) * -1) : (x))
-
 #define WIN_WIDTH 600
 #define WIN_HEIGHT 400
 
-#define BAR_POSX 0
-#define BAR_POSY 0
-
-#define SQR_POSX WIN_WIDTH / 5
-#define SQR_POSY 0
-
 #define GREY 0x00A2B5BF
-
-typedef struct	s_bresenham
-{
-	int	dx;
-	int	dy;
-	int	xe;
-	int	ye;
-}				t_bresenham;
 
 typedef struct	s_img
 {
@@ -74,9 +58,9 @@ typedef struct	s_clrpick
 	t_click		click;
 }				t_clrpick;
 
-void		img_pixel_put(t_img *img, int x, int y, int color)
+void			img_pixel_put(t_img *img, int x, int y, int color)
 {
-	int		*i;
+	int			*i;
 
 	if (x >= 0 && x < img->x_size && y >= 0 && y < img->y_size)
 	{
@@ -84,36 +68,7 @@ void		img_pixel_put(t_img *img, int x, int y, int color)
 		i = i + y * img->x_size + x;
 		*i = color;
 	}
-	else
-		printf("Depassement image\n");
 }
-
-/*void	ft_draw_segment(int x1, int y1, int x2, int y2, t_clrpick *clrpick)
-{
-	t_bresenham	err;
-
-	err.ye = y2 - y1;
-	err.xe = x2 - x1;
-	err.dx = err.xe * 2;
-	err.dy = err.ye * 2;
-	while (((err.dx > 0) ? x1 < x2 : x2 < x1)
-		|| ((err.dy > 0) ? y1 < y2 : y1 > y2))
-	{
-		img_pixel_put(&(clrpick->bar_cursor), x1, y1, clrpick->color.color);
-		err.ye -= ABS(err.dx);
-		err.xe -= ABS(err.dy);
-		if (err.ye <= 0)
-		{
-			((err.dx < 0) ? --x1 : ++x1);
-			err.ye += ABS(err.dy);
-		}
-		if (err.xe < 0)
-		{
-			((err.dy < 0) ? --y1 : ++y1);
-			err.xe += ABS(err.dx);
-		}
-	}
-}*/
 
 void			img_draw_line(t_img *img, int y, int color)
 {
@@ -171,21 +126,12 @@ int				ft_bar(t_clrpick *clrpick)
 	return (0);
 }
 
-int				ft_bar_cursor(void *mlx_ptr, t_clrpick *clrpick)
+void			ft_draw_bar_cursor(t_clrpick *clrpick)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	clrpick->bar_cursor.x_size = clrpick->bar.x_size + 4;
-	clrpick->bar_cursor.y_size = 7;
-	clrpick->bar_cursor.x = clrpick->bar.x - ((clrpick->bar_cursor.x_size - clrpick->bar.x_size) / 2);
-	clrpick->bar_cursor.y = -(clrpick->bar.y_size / 2);
-	clrpick->bar_cursor.ptr = mlx_new_image(mlx_ptr, clrpick->bar_cursor.x_size,
-											clrpick->bar_cursor.y_size);
-	clrpick->bar_cursor.data =
-		mlx_get_data_addr(clrpick->bar_cursor.ptr, &(clrpick->bar_cursor.bpp),
-						&(clrpick->bar_cursor.sl), &(clrpick->bar_cursor.endian));
 	j = -1;
 	while (++j < clrpick->bar_cursor.y_size)
 	{
@@ -195,23 +141,40 @@ int				ft_bar_cursor(void *mlx_ptr, t_clrpick *clrpick)
 				img_pixel_put(&(clrpick->bar_cursor), i, j, GREY);
 		else
 			while (++i < clrpick->bar_cursor.x_size)
-				img_pixel_put(&(clrpick->bar_cursor), i, j, ((i < 2 || i >= clrpick->bar_cursor.x_size - 2) ? GREY : 0xFF000000));
+				img_pixel_put(&(clrpick->bar_cursor), i, j, ((i < 2 ||
+					i >= clrpick->bar_cursor.x_size - 2) ? GREY : 0xFF000000));
 	}
+}
+
+int				ft_bar_cursor(t_clrpick *clrpick)
+{
+	clrpick->bar_cursor.x_size = clrpick->bar.x_size + 4;
+	clrpick->bar_cursor.y_size = 7;
+	clrpick->bar_cursor.x = clrpick->bar.x - ((clrpick->bar_cursor.x_size -
+		clrpick->bar.x_size) / 2);
+	clrpick->bar_cursor.y = -(clrpick->bar_cursor.y_size / 2);
+	if (!(clrpick->bar_cursor.ptr = mlx_new_image(clrpick->mlx_ptr,
+		clrpick->bar_cursor.x_size, clrpick->bar_cursor.y_size)) ||
+	!(clrpick->bar_cursor.data = mlx_get_data_addr(clrpick->bar_cursor.ptr,
+		&(clrpick->bar_cursor.bpp), &(clrpick->bar_cursor.sl),
+		&(clrpick->bar_cursor.endian))))
+		return (1);
+	ft_draw_bar_cursor(clrpick);
 	return (0);
 }
 
 int				ft_draw_sqr(t_clrpick *clrpick, t_color color)
 {
-	int		k;
-	int		x;
-	int		y;
-	t_color	pxl_clr;
+	int			k;
+	int			x;
+	int			y;
+	t_color		pxl_clr;
 
-	y = 0;
-	while (y < clrpick->sqr.y_size)
+	y = -1;
+	while (++y < clrpick->sqr.y_size)
 	{
-		x = 0;
-		while (x < clrpick->sqr.x_size)
+		x = -1;
+		while (++x < clrpick->sqr.x_size)
 		{
 			k = -1;
 			while (++k < 3)
@@ -223,28 +186,63 @@ int				ft_draw_sqr(t_clrpick *clrpick, t_color color)
 			}
 			pxl_clr.byte[3] = 0;
 			img_pixel_put(&(clrpick->sqr), x, y, pxl_clr.color);
-			++x;
 		}
-		++y;
 	}
 	return (0);
 }
 
-int				ft_sqr(t_clrpick *clrpick, t_color color)
+int				ft_sqr(t_clrpick *clrpick)
 {
-	if (!(clrpick->sqr.ptr))
+	clrpick->sqr.x_size = WIN_WIDTH / 3 * 2;
+	clrpick->sqr.y_size = WIN_HEIGHT;
+	clrpick->sqr.x = WIN_WIDTH / 5;
+	clrpick->sqr.y = 0;
+	if (!(clrpick->sqr.ptr = mlx_new_image(clrpick->mlx_ptr,
+		clrpick->sqr.x_size, clrpick->sqr.y_size)) ||
+		!(clrpick->sqr.data = mlx_get_data_addr(clrpick->sqr.ptr,
+		&(clrpick->sqr.bpp), &(clrpick->sqr.sl), &(clrpick->sqr.endian))))
+		return (1);
+	ft_draw_sqr(clrpick, (t_color)0xFF0000);
+	return (0);
+}
+
+void			ft_draw_sqr_cursor(t_clrpick *clrpick)
+{
+	int			i;
+	int			j;
+
+	j = 0;
+	while (j < clrpick->sqr_cursor.y_size)
 	{
-		clrpick->sqr.x_size = WIN_WIDTH / 3 * 2;
-		clrpick->sqr.y_size = WIN_HEIGHT;
-		clrpick->sqr.x = WIN_WIDTH / 5;
-		clrpick->sqr.y = 0;
-		if (!(clrpick->sqr.ptr = mlx_new_image(clrpick->mlx_ptr,
-			clrpick->sqr.x_size, clrpick->sqr.y_size)) ||
-			!(clrpick->sqr.data = mlx_get_data_addr(clrpick->sqr.ptr,
-			&(clrpick->sqr.bpp), &(clrpick->sqr.sl), &(clrpick->sqr.endian))))
-			return (1);
+		i = 0;
+		while (i < clrpick->sqr_cursor.x_size)
+		{
+			if ((i >= clrpick->sqr_cursor.x_size / 2 - 1 &&
+				i <= clrpick->sqr_cursor.x_size / 2 + 1) ||
+				(j >= clrpick->sqr_cursor.y_size / 2 - 1 &&
+				j <= clrpick->sqr_cursor.y_size / 2 + 1))
+				img_pixel_put(&(clrpick->sqr_cursor), i, j, 0x42000000 | GREY);
+			else
+				img_pixel_put(&(clrpick->sqr_cursor), i, j, 0xFF000000);
+			i++;
+		}
+		j++;
 	}
-	ft_draw_sqr(clrpick, (t_color)color);
+}
+
+int				ft_sqr_cursor(t_clrpick *clrpick)
+{
+	clrpick->sqr_cursor.x_size = 15;
+	clrpick->sqr_cursor.y_size = clrpick->sqr_cursor.x_size;
+	clrpick->sqr_cursor.x = clrpick->sqr.x - clrpick->sqr_cursor.x_size / 2;
+	clrpick->sqr_cursor.y = clrpick->sqr.y - clrpick->sqr_cursor.y_size / 2;
+	if (!(clrpick->sqr_cursor.ptr = mlx_new_image(clrpick->mlx_ptr,
+		clrpick->sqr_cursor.x_size, clrpick->sqr_cursor.y_size)) ||
+		!(clrpick->sqr_cursor.data = mlx_get_data_addr(clrpick->sqr_cursor.ptr,
+		&(clrpick->sqr_cursor.bpp), &(clrpick->sqr_cursor.sl),
+		&(clrpick->sqr_cursor.endian))))
+		return (1);
+	ft_draw_sqr_cursor(clrpick);
 	return (0);
 }
 
@@ -267,9 +265,14 @@ int				ft_isinimage(t_img *img, int x, int y)
 void			ft_draw_window(t_clrpick *clrpick)
 {
 	mlx_clear_window(clrpick->mlx_ptr, clrpick->win_ptr);
-	mlx_put_image_to_window(clrpick->mlx_ptr, clrpick->win_ptr, clrpick->bar.ptr, clrpick->bar.x, clrpick->bar.y);
-	mlx_put_image_to_window(clrpick->mlx_ptr, clrpick->win_ptr, clrpick->sqr.ptr, clrpick->sqr.x, clrpick->sqr.y);
-	mlx_put_image_to_window(clrpick->mlx_ptr, clrpick->win_ptr, clrpick->bar_cursor.ptr, clrpick->bar_cursor.x, clrpick->bar_cursor.y);
+	mlx_put_image_to_window(clrpick->mlx_ptr, clrpick->win_ptr,
+		clrpick->bar.ptr, clrpick->bar.x, clrpick->bar.y);
+	mlx_put_image_to_window(clrpick->mlx_ptr, clrpick->win_ptr,
+		clrpick->sqr.ptr, clrpick->sqr.x, clrpick->sqr.y);
+	mlx_put_image_to_window(clrpick->mlx_ptr, clrpick->win_ptr,
+		clrpick->bar_cursor.ptr, clrpick->bar_cursor.x, clrpick->bar_cursor.y);
+	mlx_put_image_to_window(clrpick->mlx_ptr, clrpick->win_ptr,
+		clrpick->sqr_cursor.ptr, clrpick->sqr_cursor.x, clrpick->sqr_cursor.y);
 }
 
 int				ft_clrpick_release_event(int button, int x, int y, void *param)
@@ -282,6 +285,8 @@ int				ft_clrpick_release_event(int button, int x, int y, void *param)
 	return (0);
 }
 
+void			ft_close_color_picker(void *clrpick_ptr);
+
 int				ft_clrpick_motion_event(int x, int y, void *param)
 {
 	t_clrpick	*clrpick;
@@ -292,14 +297,33 @@ int				ft_clrpick_motion_event(int x, int y, void *param)
 		if (ft_isinimage(&(clrpick->bar), clrpick->click.x, clrpick->click.y))
 		{
 			if (y < clrpick->bar.y)
-				clrpick->bar_cursor.y = clrpick->bar.y;
+				y = clrpick->bar.y;
 			else if (y >= clrpick->bar.y + clrpick->bar.y_size)
-				clrpick->bar_cursor.y = clrpick->bar.y + clrpick->bar.y_size - 1;
-			else
-				clrpick->bar_cursor.y = y;
-			ft_draw_sqr(clrpick, (t_color)img_get_color(&(clrpick->bar), 0, clrpick->bar_cursor.y));
-			clrpick->bar_cursor.y -= clrpick->bar_cursor.y_size / 2;
+				y = clrpick->bar.y + clrpick->bar.y_size - 1;
+			clrpick->bar_cursor.y = y - clrpick->bar_cursor.y_size / 2;
+			ft_draw_sqr(clrpick, img_get_color(&(clrpick->bar), 0, y));
 			ft_draw_window(clrpick);
+			clrpick->f(img_get_color(&(clrpick->sqr), clrpick->sqr_cursor.x + 
+				clrpick->sqr_cursor.x_size / 2 - clrpick->sqr.x,
+				clrpick->sqr_cursor.y + clrpick->sqr_cursor.y_size / 2 -
+				clrpick->sqr.y).color,
+				clrpick->param);
+		}
+		if (ft_isinimage(&(clrpick->sqr), clrpick->click.x, clrpick->click.y))
+		{
+			if (x < clrpick->sqr.x)
+				x = clrpick->sqr.x;
+			else if (x >= clrpick->sqr.x + clrpick->sqr.x_size)
+				x = clrpick->sqr.x + clrpick->sqr.x_size - 1;
+			if (y < clrpick->sqr.y)
+				y = clrpick->sqr.y;
+			else if (y >= clrpick->sqr.y + clrpick->sqr.y_size)
+				y = clrpick->sqr.y + clrpick->sqr.y_size - 1;
+			clrpick->sqr_cursor.x = x - clrpick->sqr_cursor.x_size / 2;
+			clrpick->sqr_cursor.y = y - clrpick->sqr_cursor.y_size / 2;
+			ft_draw_window(clrpick);
+			clrpick->f(img_get_color(&(clrpick->sqr), x - clrpick->sqr.x, y - clrpick->sqr.y).color,
+				clrpick->param);
 		}
 	}
 	return (0);
@@ -332,13 +356,11 @@ void			ft_bzero(void *ptr, size_t len)
 	}
 }
 
-void			*mlx_color_picker(void *mlx_ptr, int (*f)(int, void *),
+void			*ft_color_picker(void *mlx_ptr, int (*f)(int, void *),
 				void *param)
 {
 	t_clrpick	*clrpick;
 
-	(void)f;
-	(void)params;
 	if (!(clrpick = (t_clrpick *)malloc(sizeof(t_clrpick))))
 		return (NULL);
 	ft_bzero(clrpick, sizeof(t_clrpick));
@@ -346,24 +368,69 @@ void			*mlx_color_picker(void *mlx_ptr, int (*f)(int, void *),
 	clrpick->f = f;
 	clrpick->param = param;
 	if (ft_bar(clrpick) ||
-		ft_bar_cursor(mlx_ptr, clrpick) ||
-		ft_sqr(clrpick, img_get_color(&(clrpick->bar), 0, clrpick->bar_cursor.y + (clrpick->bar_cursor.y_size / 2))))
-//		mlx_color_sqr_cursor(mlx_ptr, clrpick))
+		ft_bar_cursor(clrpick) ||
+		ft_sqr(clrpick) ||
+		ft_sqr_cursor(clrpick))
 		return (NULL);
-	clrpick->win_ptr = mlx_new_window(mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "test");
-	mlx_put_image_to_window(mlx_ptr, clrpick->win_ptr, clrpick->bar.ptr, clrpick->bar.x, clrpick->bar.y);
-	mlx_mouse_hook(clrpick->win_ptr, &ft_clrpick_click_event, clrpick);
-	mlx_hook(clrpick->win_ptr, MotionNotify, PointerMotionMask, &ft_clrpick_motion_event, clrpick);
-	mlx_hook(clrpick->win_ptr, ButtonRelease, ButtonReleaseMask, &ft_clrpick_release_event, clrpick);
-	mlx_loop(mlx_ptr);
+	clrpick->win_ptr = mlx_new_window(mlx_ptr, WIN_WIDTH,
+		WIN_HEIGHT, "color_picker");
+	ft_draw_window(clrpick);
+	mlx_hook(clrpick->win_ptr, ButtonPress, ButtonPressMask,
+		&ft_clrpick_click_event, clrpick);
+	mlx_hook(clrpick->win_ptr, MotionNotify, PointerMotionMask,
+		&ft_clrpick_motion_event, clrpick);
+	mlx_hook(clrpick->win_ptr, ButtonRelease, ButtonReleaseMask,
+		&ft_clrpick_release_event, clrpick);
 	return (clrpick);
+}
+
+void			ft_close_color_picker(void *clrpick_ptr)
+{
+	t_clrpick	*clrpick;
+
+	clrpick = clrpick_ptr;
+	mlx_destroy_image(clrpick->mlx_ptr, clrpick->bar.ptr);
+	mlx_destroy_image(clrpick->mlx_ptr, clrpick->sqr.ptr);
+	mlx_destroy_image(clrpick->mlx_ptr, clrpick->bar_cursor.ptr);
+	mlx_destroy_image(clrpick->mlx_ptr, clrpick->sqr_cursor.ptr);
+	mlx_destroy_window(clrpick->mlx_ptr, clrpick->win_ptr);
+	free(clrpick);
+}
+
+int				test(int color, void *param)
+{
+	t_clrpick	*clrpick;
+	int			i;
+	int			j;
+
+	clrpick = param;
+	j = 0;
+	while (j < WIN_HEIGHT)
+	{
+		i = 0;
+		while (i < WIN_WIDTH)
+		{
+			img_pixel_put(&(clrpick->bar), i, j, color);
+			i++;
+		}
+		j++;
+	}
+	mlx_put_image_to_window(clrpick->mlx_ptr, clrpick->win_ptr, clrpick->bar.ptr, 0, 0);
+	return (0);
 }
 
 int				main(void)
 {
-	void		*mlx_ptr;
+	t_clrpick	clrpick;
+	void		*clrpick_ptr;
 
-	mlx_ptr = mlx_init();
-	mlx_color_picker(mlx_ptr, NULL, NULL);
+	clrpick.mlx_ptr = mlx_init();
+	clrpick.win_ptr = mlx_new_window(clrpick.mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "test");
+	clrpick.bar.ptr = mlx_new_image(clrpick.mlx_ptr, WIN_WIDTH, WIN_HEIGHT);
+	clrpick.bar.data = mlx_get_data_addr(clrpick.bar.ptr, &(clrpick.bar.bpp), &(clrpick.bar.sl), &(clrpick.bar.endian));
+	clrpick.bar.x_size = WIN_WIDTH;
+	clrpick.bar.y_size = WIN_HEIGHT;
+	clrpick_ptr = ft_color_picker(clrpick.mlx_ptr, &test, &clrpick);
+	mlx_loop(clrpick.mlx_ptr);
 	return (0);
 }
